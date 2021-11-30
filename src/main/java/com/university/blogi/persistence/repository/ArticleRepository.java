53be2fed0b6e6335ee18180ab2b76d078efade50
Szakacs.Agi <szakacs.agi21@gmail.com>
2021-11-30 10:09:59 +0100

    Konkret cikk vegpont implementalasa

---
+++ src/main/java/com/university/blogi/persistence/repository/ArticleRepository.java
@@ -0,0 +1,11 @@
+package com.university.blogi.persistence.repository;
+
+import com.university.blogi.persistence.entity.ArticleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<ArticleEntity, UUID> {
+}
