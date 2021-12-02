c558522996ed14bf186c76c8fdbff4bd7764de23
SzakacsAgi <szakacs.agi21@gmail.com>
2021-12-02 22:33:17 +0100

    eldobni

--- src/main/resources/static/js/homePageGetData.js
+++ src/main/resources/static/js/homePageGetData.js
@@ -1,111 +1,17 @@
-function createNode(element) {
-    return document.createElement(element);
-}
-
-function append(parent, el) {
-  return parent.appendChild(el);
-}
-
+var imported = document.createElement('script');
+imported.src = 'createPreviewContent.js';
+document.head.appendChild(imported);
 function loadData() {
-  const div = document.getElementById('card-image0');
-  const url = 'http://localhost:8080/blogi/api/articles';
-  fetch(url)
-  .then((resp) => resp.json())
-  .then(function(data) {
-  let articles_data = data.articles;
-  console.log(articles_data);
-  let index = 0;
-  return articles_data.map(function(article) {
-    console.log(article);
-    console.log(index);
-    if(index == 0){
-        let img = createNode('img');
-        img.src = article.imageUrl;
-        img.classList.add("card-img-top");
-        let cardBody = createNode("div");
-        cardBody.classList.add("card-body");
-        cardBody.id = "card-body";
-        let title = createNode("h5");
-        title.classList.add("card-title");
-        title.innerHTML = `${article.title}`;
-        let previewContent = createNode("p");
-        previewContent.classList.add("card-text");
-        previewContent.innerHTML = `${article.previewContent}`;
-        let date = createNode("p");
-        date.classList.add("creation-date");
-        date.innerHTML = `Megjelenés: ${article.creationDate}`;
-        let button = createNode("a");
-        button.classList.add("btn");
-        button.classList.add("btn-primary");
-        button.href.innerHTML = `http://localhost:8080/blogi/api/articles/${article.id}`;
-        button.innerHTML = "Elolvasom";
-        append(div, img);
-        append(div, cardBody);
-        append(cardBody, title);
-        append(cardBody, previewContent);
-        append(cardBody, date);
-        append(cardBody, button)
-    }
-
-    if(index == 1){
-        const div = document.getElementById('card-image1');
-        let img = createNode('img');
-                img.src = article.imageUrl;
-                img.classList.add("card-img-top");
-                let cardBody = createNode("div");
-                cardBody.classList.add("card-body");
-                cardBody.id = "card-body";
-                let title = createNode("h5");
-                title.classList.add("card-title");
-                title.innerHTML = `${article.title}`;
-                let previewContent = createNode("p");
-                previewContent.classList.add("card-text");
-                previewContent.innerHTML = `${article.previewContent}`;
-                let date = createNode("p");
-                date.classList.add("creation-date");
-                date.innerHTML = `Megjelenés: ${article.creationDate}`;
-                let button = createNode("a");
-                button.classList.add("btn");
-                button.classList.add("btn-primary");
-                button.href.innerHTML = `http://localhost:8080/blogi/api/articles/${article.id}`;
-                button.innerHTML = "Elolvasom";
-                append(div, img);
-                append(div, cardBody);
-                append(cardBody, title);
-                append(cardBody, previewContent);
-                append(cardBody, date);
-                append(cardBody, button)
-    }
-    if(index == 2){
-            const div = document.getElementById('card-image2');
-            let img = createNode('img');
-                    img.src = article.imageUrl;
-                    img.classList.add("card-img-top");
-                    let cardBody = createNode("div");
-                    cardBody.classList.add("card-body");
-                    cardBody.id = "card-body";
-                    let title = createNode("h5");
-                    title.classList.add("card-title");
-                    title.innerHTML = `${article.title}`;
-                    let previewContent = createNode("p");
-                    previewContent.classList.add("card-text");
-                    previewContent.innerHTML = `${article.previewContent}`;
-                    let date = createNode("p");
-                    date.classList.add("creation-date");
-                    date.innerHTML = `Megjelenés: ${article.creationDate}`;
-                    let button = createNode("a");
-                    button.classList.add("btn");
-                    button.classList.add("btn-primary");
-                    button.href = "#";
-                    button.innerHTML = "Elolvasom";
-                    append(div, img);
-                    append(div, cardBody);
-                    append(cardBody, title);
-                    append(cardBody, previewContent);
-                    append(cardBody, date);
-                    append(cardBody, button)
-        }
-       index += 1;
+    const url = 'http://localhost:8080/blogi/api/articles';
+    fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+     let articles_data = data.articles;
+     console.log(articles_data);
+     let index = 0;
+     return articles_data.map(function(article) {
+        createPreviewContent(article, index)
+        index += 1;
   })
   })
   .catch(function(error) {
