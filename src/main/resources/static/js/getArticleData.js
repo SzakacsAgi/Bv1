636e922672d98e2a9d32e70d07d7f66dcab50a26
SzakacsAgi <szakacs.agi21@gmail.com>
2021-12-09 14:53:03 +0100

    felesleges importok törlése

--- src/main/resources/static/js/getArticleData.js
+++ src/main/resources/static/js/getArticleData.js
@@ -1,14 +1,9 @@
-var imported = document.createElement("script");
-imported.src = "createArticleContent.js";
-document.head.appendChild(imported);
-
 function loadArticleData() {
    var id = localStorage.getItem("id");
     const url = 'http://localhost:8080/blogi/api/articles/' + id;
     fetch(url)
     .then((resp) => resp.json())
     .then(function(data) {
-     console.log(data);
      createArticleContent(data);
   })
   .catch(function(error) {
