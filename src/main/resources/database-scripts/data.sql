0f641d74af85fa0d66547c012670184734001b68
SzakacsAgi <61914845+SzakacsAgi@users.noreply.github.com>
2021-12-12 14:38:56 +0100

    Update data.sql
    Tartalom frissítése

--- src/main/resources/database-scripts/data.sql
+++ src/main/resources/database-scripts/data.sql
@@ -2,7 +2,7 @@ INSERT INTO ARTICLES (id, title, author_name, preview_content, content, image_ur
   ('4cf059db-e3bf-4bd4-b6be-395e5179aca8',
   'Talán már magától értetődő általános utazási tippek indulás előttre', 'András',
   'Mindig rájövök arra, hogy a sok év alatt tapasztalatként összegyűjtött, számomra már magától értetődő tippek és teendők egy kevésbé tapasztalt utazónak mennyire nem egyértelműek. Ezért összegyűjtöttem őket ide nektek egy csokorba.',
-  '<div class = “container”> <p> Ez lesz a cím </p> </div>',
+  '<div class = “container”> <p> Ez lesz a tartalom </p> </div>',
   'https://i.pinimg.com/originals/a0/d5/15/a0d5157efeec8b765326fc1184509ab1.png',
   CAST('2021-07-30 10:43:28' AS DATETIME));
 
@@ -13,7 +13,7 @@ INSERT INTO ARTICLES (id, title, author_name, preview_content, content, image_ur
   ('818d1ba0-3a04-48c3-a409-5922630b21c6',
   'A legjobb repülőjegy kereső oldalak összehasonlítása', 'András',
   'Melyik a legjobb repülőjegy kereső oldal? Nincs olyan, az egyik ebben jó, a másik abban. Ebben a felsorolásban bemutatom az általam legjobbaknak tartott oldalak előnyeit és hátrányait.',
-  '<div class = “container”> <p> Ez lesz a cím </p> </div>',
+  '<div class = “container”> <p> Ez lesz a tartalom </p> </div>',
   'https://thumbs.dreamstime.com/b/two-airline-tickets-airplane-boarding-ticket-passenger-name-airlines-flight-invitation-airplanes-pass-vector-traveler-164126383.jpg_',
   CAST('2021-06-13 14:22:12' AS DATETIME));
 
@@ -45,7 +45,7 @@ INSERT INTO ARTICLES (id, title, author_name, preview_content, content, image_ur
   ('d71ab202-abab-4623-a395-436f4c5bfab2',
   'Hol tudsz olcsó utasbiztosítást kötni?', 'András',
   'Azt hiszem senkinek nem kell azt magyaráznom, hogy miért fontos biztosítást kötni. Elmesélhetnék itt 1-2, akár több balszerencsés kimenetelü története, de nem fogok. Csak annyit mondok: utas biztosítás nélkül tilos útnak indulni!',
-  '<div class = "container"> <p> Ez lesz a cím </p> </div>',
+  '<div class = "container"> <p> Ez lesz a tartalom </p> </div>',
   'https://utazaskatalogus.hu/wp-content/uploads/2019/07/utasbiztositas-kulfoldon.jpg',
   CAST('2021-08-08 09:43:54' AS DATETIME));
 
